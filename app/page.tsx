a0ac3e2b85c20113c167ce40565f22375b38c7d0
seung-gumon <b9327912@gmail.com>
2023-04-02 00:28:04 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         </p>
         <div>
           <a
+
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
