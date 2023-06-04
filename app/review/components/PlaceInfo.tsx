cdd9ff84200287ac7d3b5955d3cb9d89bcd346bd
seung-gumon <64651532+seung-gumon@users.noreply.github.com>
2023-06-05 06:52:23 +0900

    Done:)

--- app/review/components/PlaceInfo.tsx
+++ app/review/components/PlaceInfo.tsx
@@ -34,9 +34,9 @@ function PlaceInfo() {
         </span>
       </p>
       <p className="py-1.5 border-t-[1px] border-b-[1px] border-gray-300 last:border-b-1 flex items-center">
-        <IoStorefrontOutline className="inline-block mr-1.5 self-start" size={36} />
+        <IoStorefrontOutline className="inline-block mr-1.5 " />
         <span className="flex items-center">
-          주말, 연휴 및 특히 비오는 날이면 재료가 금새 소진되어 영업시간이 한 두 시간 앞당겨 일찍 ㅎㅇㅎㅇㅎㅇ 핼로 안녕 잘가잘가
+          주말, 연휴 및 특히 비오는 날이면 재료가 금새 소진되어
         </span>
       </p>
     </section>
